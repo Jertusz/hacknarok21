b79bdb947741f4f4c252656e75a75dcd063fa62f
Paweł Pankiewicz <pawelp6020@gmail.com>
2021-03-27 23:02:06 +0100

    Progres on Websocket layer

--- frontend/src/background/background.ts
+++ frontend/src/background/background.ts
@@ -1,21 +1,38 @@
+import { ALL_EVENTS } from './EventTypes';
 import WS_MESSAGE_TYPES from './WSMessageTypes';
 
 let ws: WebSocket;
+const eventListeners = new Map();
+for (const item in ALL_EVENTS) {
+    eventListeners.set(item, []);
+}
+console.log(eventListeners);
 
-browser.runtime.onMessage.addListener(function (request: { type: WS_MESSAGE_TYPES; content: string }, sender, sendResponse) {
+browser.runtime.onMessage.addListener(function (
+    request: { type: WS_MESSAGE_TYPES; content: any },
+    sender,
+    sendResponse
+) {
     switch (request.type) {
         case WS_MESSAGE_TYPES.CONNECT: {
             if (ws !== undefined) ws.close();
             ws = new WebSocket(request.content);
             break;
         }
-        case WS_MESSAGE_TYPES.SEND: {
-            ws.send(request.content);
+        case WS_MESSAGE_TYPES.SEND_GENERIC: {
+            ws.send(JSON.stringify(request.content));
+            break;
+        }
+        case WS_MESSAGE_TYPES.SEND_EVENT: {
+            ws.send(JSON.stringify(request.content));
             break;
         }
         case WS_MESSAGE_TYPES.DISCONNECT: {
             ws.close();
             break;
+        }
+        case WS_MESSAGE_TYPES.REGISTER: {
+            eventListeners.set(request.content.event, request.content.callback);
         }
     }
 
