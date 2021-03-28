5a6ecab2aa41bb71dcc5102d066ba60ad27c0a5e
Jedrzej Szadejko <jendziu@gmail.com>
2021-03-28 07:03:35 +0200

    Making docker work

--- backend/settings/asgi.py
+++ backend/settings/asgi.py
@@ -9,14 +9,16 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
-from channels.routing import URLRouter
 from django.core.asgi import get_asgi_application
+import django
 
-import api.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.settings")
+django.setup()
 
+from channels.routing import ProtocolTypeRouter
+from channels.routing import URLRouter
+import api.routing
 application = ProtocolTypeRouter(
     {"http": get_asgi_application(), "websocket": URLRouter(api.routing.websocket_urls)}
 )
